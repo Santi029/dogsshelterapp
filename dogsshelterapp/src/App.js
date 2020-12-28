1956403b3f580001c23c1313e023001c783fbab1
Hector L. Santiago <hlsp@me.com>
2020-12-28 01:49:46 -0600

    Adding css and imports to app.js.

--- dogsshelterapp/src/App.js
+++ dogsshelterapp/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
 import React from 'react';
 import './App.scss';
 import { Navbar, Nav, NavDropdown, Form,  FormControl, Button, Table} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
